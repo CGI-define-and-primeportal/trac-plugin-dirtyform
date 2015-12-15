c0143c3fe9860f137950c51df20446f129486393
Nick Piper <nick.piper@cgi.com>
2015-12-15 17:27:12 +0000

    Tidy up license and copyright strings, adding maintainer/contact details. #5817
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@26030 484df14f-75d2-4b3f-8ba0-490e05069a01

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010, Logica
+# Copyright (c) 2015 CGI
 # Contains code Copyright 2009 Asa Wilson
 
 from setuptools import setup
@@ -7,10 +7,15 @@ setup(
     name = 'DirtyFormPlugin',
     version = '0.1',
     author = 'Nick Piper',
-    author_email = 'nick.piper@logica.com',
+    author_email = 'nick.piper@cgi.com',
+    maintainer="CGI CoreTeam",
+    maintainer_email="coreteam.service.desk.se@cgi.com",
+    contact="CGI CoreTeam",
+    contact_email="coreteam.service.desk.se@cgi.com",
+    classifiers=['License :: OSI Approved :: BSD License'],
+    license='BSD',
+    url='http://define.primeportal.com/',
     description = 'Adds a warning if the user might lose changes to a form.',
-    license =  """Copyright (c) 2010, Logica. Released under the MIT license. """,
-    url = "http://define4.trac.uk.logica.com",
     packages = ['dirtyformplugin'],
     package_data = {'dirtyformplugin' : ['htdocs/css/*.css', 'htdocs/js/*.js']}, 
     entry_points = {'trac.plugins': ['dirtyformplugin = dirtyformplugin']},
